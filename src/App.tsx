10f53da45fcfb03d81b9aa3cb98e8b2d3c788670
earthgohan <85003405+earthgohan@users.noreply.github.com>
2021-09-10 00:07:31 +0530

    Update window earth

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import React, { useEffect } from 'react';
 declare global {
   interface Window {
     earth: unknown;
+    ic: any;
   }
 }
 
@@ -28,10 +29,10 @@ function App() {
     const handleEarthEnable = async () => {
       getEarth().then(async (earth) => {
         console.log('gotEarth', earth)
-          // @ts-ignore: Object is of type 'unknown'.
+        // @ts-ignore: Object is of type 'unknown'.
         let hello = await earth?.isConnected();
         console.log('gotEarth', hello)
-          // @ts-ignore: Object is of type 'unknown'.
+        // @ts-ignore: Object is of type 'unknown'.
         console.log(await earth?.enable())
       });
 
@@ -47,51 +48,23 @@ function App() {
             });
        */
     };
+
+
     handleEarthEnable();
   }, []);
 
   return (
     <div className={styles.app}>
       <div className={styles.cardcont}>
-        <div className={styles.cardcontinner}>
-          <div className={styles.cardinfo}>
-            <div className={styles.pillet}>Earth Profile #</div>
-            <div className={styles.value}>$209,092.22</div>
-            <div className={styles.stats}>+12.34%</div>
-          </div>
-        </div>
-        <div className={styles.cardnetworks}>
-          <div className={styles.networktitle}>Networks</div>
 
-          {[1, 2, 3].map((net, index) => <div
-            className={styles.netcard}
-            key={index}>
-            <div className={styles.networklogo}></div>
-            <div className={styles.netmid}>
-              <div className={styles.netname}>
-                Internet Computer
-              </div>
-              <div className={styles.netassets}>
-                5 Assets
-              </div>
-            </div>
 
-            <div className={styles.netlast}>
-              <div className={styles.netvalue}>
-                $39,092.22 USD
-              </div>
-              <div className={styles.netstats}>
-                +0.34%
-              </div>
-            </div>
-          </div>)}
+        Demo application
 
 
 
-        </div>
       </div>
-
     </div>
+
   );
 }
 
